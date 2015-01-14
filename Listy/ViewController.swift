bef09d8edf1df3d14b013dcd217584e0a6f29590
Andrew Hulsizer <andrew@tastemade.com>
2015-01-14 13:22:52 -0600

    Initial Commit

---
+++ Listy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Listy
+//
+//  Created by Andrew Hulsizer on 1/14/15.
+//  Copyright (c) 2015 SwiftYeti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
